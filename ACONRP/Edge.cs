c4aa3ab144cc9166f8c186fe502df4211670c212
Alberto Pes <pes.alberto@outlook.it>
2018-12-19 15:59:31 +0100

    Impostazione classi e metodi per la generazione dei pattern, aggiunta commenti

---
+++ ACONRP/Edge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACONRP
+{
+    class Edge
+    {
+        private int indexNurseA;
+        private int indexNurseB;
+        private int indexNodeA;
+        private int indexNodeB;
+        private double pheromone;
+    }
+}
